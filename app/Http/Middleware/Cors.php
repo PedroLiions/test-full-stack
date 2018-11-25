fa43e6e5747d173231e94663d2e8594607c027b5
Pedro Lima <pedrolion1@hotmail.com>
2018-11-25 14:06:19 -0200

    Ajustes no middleware de cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,25 +17,14 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-//        header('Access-Control-Allow-Origin', '*');
-//        header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-//        header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
-//        return $next($request);
-
-        /* @var $response Response */
-        $response = $next($request);
-        if (!$request->isMethod('OPTIONS')) {
-            return $response;
+        if ($request->isMethod('OPTIONS')) {
+            $response = Response::make();
+        } else {
+            $response = $next($request);
         }
-        $allow = $response->headers->get('Allow'); // true list of allowed methods
-        if (!$allow) {
-            return $response;
-        }
-        $headers = [
-            'Access-Control-Allow-Methods' => $allow,
-            'Access-Control-Max-Age' => 3600,
-            'Access-Control-Allow-Headers' => 'X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept',
-        ];
-        return $response->withHeaders($headers);
+        return $response
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
     }
 }
