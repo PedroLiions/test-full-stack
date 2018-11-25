13a7081682f1ccebdd2f87f9970d5b27c87af7ed
Pedro Lima <pedrolion1@hotmail.com>
2018-11-25 14:07:54 -0200

    Ajustes no middleware de cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,16 +17,14 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: http://cappuccino.plima.me/login/');
-
         if ($request->isMethod('OPTIONS')) {
             $response = Response::make();
         } else {
             $response = $next($request);
         }
         return $response
-            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application')
+            ->header('Access-Control-Allow-Origin: http://cappuccino.plima.me/login/');
     }
 }
