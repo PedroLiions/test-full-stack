515f90debc8de0cb5684ed8e5ba8ced0cb2e32d4
Pedro Lima <pedrolion1@hotmail.com>
2018-11-25 18:33:30 -0200

    Angular integrado a aplicação

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,28 @@
 //    return response('',200);
 //}])->where('any', '.*');
 
-Route::get('/', function() {
+Route::any('{all}', function() {
     View::addExtension('html', 'php');
     return View::make('index');
 });
 
-// FILES
-Route::get('get-image/{path}', 'FileController@getImage');
-
-// AUTH
-Route::post('login', 'AuthController@login');
-Route::post('register', 'AuthController@register');
-
-Route::middleware(['cors', 'jwt.auth'])->group(function() {
-    // DASHBOARD
-    Route::get('dashboard', 'DashboardController@index');
-
-    // PRODUCTS
-    Route::resource('products', 'ProductController');
-    Route::resource('product/categories', 'CategoryProductController');
-
-    // POST
-    Route::resource('posts', 'PostController');
-    Route::resource('post/categories', 'CategoryPostController');
-});
+//
+//// FILES
+//Route::get('get-image/{path}', 'FileController@getImage');
+//
+//// AUTH
+//Route::post('login', 'AuthController@login');
+//Route::post('register', 'AuthController@register');
+//
+//Route::middleware(['jwt.auth'])->group(function() {
+//    // DASHBOARD
+//    Route::get('dashboard', 'DashboardController@index');
+//
+//    // PRODUCTS
+//    Route::resource('products', 'ProductController');
+//    Route::resource('product/categories', 'CategoryProductController');
+//
+//    // POST
+//    Route::resource('posts', 'PostController');
+//    Route::resource('post/categories', 'CategoryPostController');
+//});
